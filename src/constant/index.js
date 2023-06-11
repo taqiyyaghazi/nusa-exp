6ca89016310022972826d5720a700530ec6dcf79
Taqiyya Ghazi <taqiyyaghazi@gmail.com>
2023-06-11 18:26:54 +0700

    hotfix api versioning

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,3 +1,5 @@
 export const SALT_ROUNDS = 10;
 
 export const ID_SIZE = 10;
+
+export const BASE_URL_API_V1 = 'http://localhost:3000/api/v1/';
--- src/services/api/authApi.js
+++ src/services/api/authApi.js
@@ -1,10 +1,11 @@
 // Need to use the React-specific entry point to allow generating React hooks
+import { BASE_URL_API_V1 } from '@/constant';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 // Define a service using a base URL and expected endpoints
 export const authApi = createApi({
     reducerPath: 'authApi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000/api/' }),
+    baseQuery: fetchBaseQuery({ baseUrl: BASE_URL_API_V1 }),
     endpoints: (build) => ({
         login: build.mutation({
             query: (body) => ({
