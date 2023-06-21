e815c6e53a72535188415dfb2a47c69943bdf5a9
Taqiyya Ghazi <taqiyyaghazi@gmail.com>
2023-06-21 08:28:50 +0700

    fixing firebase config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
+// import { getAnalytics } from 'firebase/analytics';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
@@ -14,6 +14,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 export const storage = getStorage();
